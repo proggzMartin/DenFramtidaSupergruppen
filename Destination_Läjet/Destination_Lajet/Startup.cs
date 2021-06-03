d8c39055c469bcd18e12c86c28889cc0a30cb50c
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-06-03 19:11:02 +0200

    Removed comments, changed database name.

--- Destination_Läjet/Destination_Lajet/Startup.cs
+++ Destination_Läjet/Destination_Lajet/Startup.cs
@@ -19,7 +19,6 @@ namespace Destination_Lajet
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -32,7 +31,6 @@ namespace Destination_Lajet
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -43,7 +41,6 @@ namespace Destination_Lajet
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
